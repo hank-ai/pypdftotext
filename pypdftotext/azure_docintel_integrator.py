59f34a0be27038624884a759295dbcfa89874e36
shartzog <sam.hartzog@hank.ai>
2025-08-02 23:54:53 -0400

    Use logging module where possible, remove private imports from pypdf

--- pypdftotext/azure_docintel_integrator.py
+++ pypdftotext/azure_docintel_integrator.py
@@ -1,46 +1,18 @@
 """Microsoft Azure Document Intelligence API Handler"""
 
 import io
-import json
+import logging
 import os
 from dataclasses import dataclass, field
-import math
-from pathlib import Path
 
 from azure.ai.documentintelligence import DocumentIntelligenceClient, AnalyzeDocumentLROPoller
 from azure.ai.documentintelligence.models import AnalyzeResult
 from azure.core.credentials import AzureKeyCredential
-from pypdf._text_extraction._layout_mode import (
-    fixed_char_width,
-    fixed_width_page,
-    y_coordinate_groups,
-)
-from pypdf._text_extraction._layout_mode._fixed_width_page import BTGroup
+from tqdm import tqdm
 
-from . import constants
+from . import constants, layout
 
-
-def _rotate_bbox(
-    p: list[float], angle: float, width: float = 8.5, height: float = 11
-) -> list[float]:
-    if abs(angle) < constants.MIN_OCR_ROTATION_DEGREES:
-        return p
-    angle = math.radians(angle)
-    _sin = math.sin(angle)
-    _cos = math.cos(angle)
-
-    def _rotate_point(x: float, y: float) -> tuple[float, float]:
-        return (
-            _cos * (x - width / 2) - _sin * (y - height / 2) + height / 2,
-            _sin * (x - width / 2) + _cos * (y - height / 2) + width / 2,
-        )
-
-    return [
-        *_rotate_point(p[0], p[1]),
-        *_rotate_point(p[2], p[3]),
-        *_rotate_point(p[4], p[5]),
-        *_rotate_point(p[6], p[7]),
-    ]
+logger = logging.getLogger(__name__)
 
 
 @dataclass
@@ -54,6 +26,7 @@ class AzureDocIntelIntegrator:
     font_height_weight: float = 1.0
     client: DocumentIntelligenceClient | None = field(default=None, init=False, repr=False)
     last_result: AnalyzeResult = field(default_factory=lambda: AnalyzeResult({}), init=False)
+    pbar_position: int | None = field(default=None, init=False, repr=False)
 
     def create_client(self) -> bool:
         """
@@ -71,23 +44,23 @@ class AzureDocIntelIntegrator:
         Returns:
             bool: True if client was created successfully. False otherwise.
         """
-        if (
-            _endpoint := os.getenv("AZURE_DOCINTEL_ENDPOINT") or constants.AZURE_DOCINTEL_ENDPOINT
-        ) and (
-            _key := os.getenv("AZURE_DOCINTEL_SUBSCRIPTION_KEY")
+        endpoint = os.getenv("AZURE_DOCINTEL_ENDPOINT") or constants.AZURE_DOCINTEL_ENDPOINT
+        key = (
+            os.getenv("AZURE_DOCINTEL_SUBSCRIPTION_KEY")
             or constants.AZURE_DOCINTEL_SUBSCRIPTION_KEY
-        ):
-            self.client = DocumentIntelligenceClient(_endpoint, AzureKeyCredential(_key))
-            constants.log(f"Azure OCR Client Created: {_endpoint=!r}")
+        )
+        if endpoint and key:
+            self.client = DocumentIntelligenceClient(endpoint, AzureKeyCredential(key))
+            logger.info("Azure OCR Client Created: endpoint='%s'", endpoint)
             return True
-        constants.log("Failed to create Azure OCR Client.")
+        logger.error("Failed to create Azure OCR Client at endpoint='%s'", endpoint)
         return False
 
     def reset(self):
         """Clear last_result and last_page_list from previous run."""
         self.last_result = AnalyzeResult({})
 
-    def ocr_pages(self, pdf: bytes, pages: list[int], debug_path: Path | None = None) -> list[str]:
+    def ocr_pages(self, pdf: bytes, pages: list[int]) -> list[str]:
         """
         Read the text from supplied pdf page indices.
 
@@ -102,75 +75,31 @@ class AzureDocIntelIntegrator:
         if constants.AZURE_DOCINTEL_AUTO_CLIENT and self.client is None:
             self.create_client()
         if self.client is None:
-            print("Azure OCR API not available. Did you create a client? Returning empty string.")
+            logger.error(
+                "Azure OCR API not available. Did you create a client? Returning empty string."
+            )
             return []
         assert self.client is not None
-        pdf_io = io.BytesIO(pdf)
-        pdf_io.seek(0)
+        logger.info("Sending pdf of %s bytes for OCR of %s pages.", len(pdf), len(pages))
         poller: AnalyzeDocumentLROPoller = self.client.begin_analyze_document(
             model_id="prebuilt-read",
-            body=pdf_io,
+            body=io.BytesIO(pdf),
             pages=",".join(str(pg + 1) for pg in pages),
         )
         self.last_result = poller.result(self.timeout)
-        results: list[str] = []
-        for idx, doc_page in enumerate(self.last_result.pages):
-            if not doc_page.lines:
-                results.append("")  # No text on this page. Add empty string and continue.
-                continue
-            bt_groups = [
-                BTGroup(
-                    tx=rotated_polygon[0] * 100,
-                    ty=rotated_polygon[1] * 100,
-                    font_size=(
-                        _fsz := (rotated_polygon[-1] - rotated_polygon[1])
-                        * constants.OCR_FONT_SIZE_MULT
-                    ),
-                    font_height=_fsz,
-                    text=_line.content,
-                    displaced_tx=rotated_polygon[2] * 100,
-                    flip_sort=-1,
-                )
-                for _line in doc_page.lines
-                if _line.polygon is not None
-                and (
-                    rotated_polygon := _rotate_bbox(
-                        _line.polygon,
-                        -(doc_page.angle or 0.0),
-                        doc_page.width or 8.5,
-                        doc_page.height or 11,
-                    )
-                )
-            ]
-            if not bt_groups:
-                results.append("")
-                continue
-            min_x = min((x["tx"] for x in bt_groups), default=0.0)
-            bt_groups = [
-                BTGroup(  # type: ignore[misc] # mypy doesn't like typedict inheritance.
-                    ogrp,
-                    tx=ogrp["tx"] - min_x,
-                    displaced_tx=ogrp["displaced_tx"] - min_x,
-                )
-                for ogrp in sorted(
-                    bt_groups, key=lambda x: (x["ty"] * x["flip_sort"], -x["tx"]), reverse=True
-                )
-                if ogrp["text"]
-            ]
-            if debug_path:
-                debug_path.joinpath(f"ocr_bts{pages[idx]}.json").write_text(
-                    json.dumps(bt_groups, indent=2, default=str), "utf-8"
-                )
-            ty_groups = y_coordinate_groups(bt_groups)
-            char_width = fixed_char_width(bt_groups)
-            results.append(
-                fixed_width_page(
-                    ty_groups,
-                    char_width,
-                    self.preserve_vertical_whitespace,
-                    self.font_height_weight,
-                )
+        logger.info("%s pages OCR'd successfully. Creating fixed width pages.", len(pages))
+        ocr_pbar = tqdm(
+            self.last_result.pages,
+            desc="Processing OCR results...",
+            disable=constants.DISABLE_PROGRESS_BAR,
+            position=self.pbar_position,
+        )
+        results: list[str] = [
+            layout.fixed_width_page(
+                doc_page, self.preserve_vertical_whitespace, self.font_height_weight
             )
+            for doc_page in ocr_pbar
+        ]
         return results
 
     def handwritten_ratio(
@@ -223,7 +152,7 @@ class AzureDocIntelIntegrator:
                 start=0,
             )
             # Guess we'll cap our value at 1.0. We should probably throw and exception here
-            # also, but again we'll fail open for now as it suite our use case.
+            # also, but again we'll fail open for now as it suites our use case.
             return min(handwritten_length / (page_end - page_start), 1.0)
 
         return 0.0
